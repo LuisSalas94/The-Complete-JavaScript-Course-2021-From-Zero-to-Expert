9569b80ddae977c41ccdebabdb91b2b3c3b5f94c
Fernando Salas <luisfernandosalasg@gmail.com>
2021-07-17 05:12:59 -0500

    Add Modal Project

--- 03-JavaScript in the browser - DOM/06-Modal/starter/script.js
+++ 03-JavaScript in the browser - DOM/06-Modal/starter/script.js
@@ -1,1 +1,37 @@
 'use strict';
+
+const modal = document.querySelector('.modal');
+const overlay = document.querySelector('.overlay');
+const btnCloseModal = document.querySelector('.close-modal');
+const btnsOpenModal = document.querySelectorAll('.show-modal');
+
+//funtion modal to close
+const closeModal = () => {
+  modal.classList.add('hidden');
+  overlay.classList.add('hidden');
+};
+
+const openModal = () => {
+  modal.classList.remove('hidden');
+  overlay.classList.remove('hidden');
+};
+
+//open modal, removing hidden class
+for (let i = 0; i < btnsOpenModal.length; i++) {
+  btnsOpenModal[i].addEventListener('click', openModal);
+}
+
+//close modal
+btnCloseModal.addEventListener('click', closeModal);
+
+//close modal without X button
+overlay.addEventListener('click', closeModal);
+
+//close modal with ESC button
+document.addEventListener('keydown', e => {
+  //only close modal wih ESC button
+  console.log(e.key);
+  if (e.key === 'Escape' && !modal.classList.contains('hidden')) {
+    closeModal();
+  }
+});
