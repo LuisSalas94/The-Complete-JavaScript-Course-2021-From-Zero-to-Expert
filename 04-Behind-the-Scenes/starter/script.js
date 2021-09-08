40b04ce0e8c9d241673d1d0c2f806650737b33ae
Fernando <luisfernandosalasg@gmail.com>
2021-09-07 22:15:47 -0500

    added behind the scenes section

--- 04-Behind-the-Scenes/starter/script.js
+++ 04-Behind-the-Scenes/starter/script.js
@@ -1,21 +1,26 @@
 'use strict';
-
-/* function calcAge(birthYear) {
+/* 
+function calcAge(birthYear) {
   const age = 2037 - birthYear;
 
   function printAge() {
-    let output = `${firstName} You are ${age}, born in ${birthYear}`;
+    let output = `${firstName}, You are ${age}, born in ${birthYear}`;
     console.log(output);
 
     if (birthYear >= 1981 && birthYear <= 1996) {
+      var millenial = true;
       const firstName = 'Luis';
       const str = `Oh, and you're a millenial, ${firstName}`;
       console.log(str);
+
       function add(a, b) {
         return a + b;
       }
-      output = 'New Output!';
+
+      output = 'NEW OUTPUT';
     }
+
+    output;
   }
 
   printAge();
@@ -25,23 +30,23 @@
 
 const firstName = 'Fernando';
 
-calcAge(1987);
+console.log(calcAge(1987));
  */
 
-//Hoisting and TDZ
-
+//variables
 //console.log(me);
-//console.log(job);
-//console.log(year);
+/* console.log(job);
+console.log(year);
+ */
 
 /* var me = 'Luis';
-let job = 'porn actor';
-const year = 1987;
+let job = 'teacher';
+const year = 1987; */
 
-//Functions
-console.log(addDecl(2, 3)); */
-//console.log(addExpr(2, 3));
-//console.log(addArrow(2, 3));
+//functions
+//console.log(addDecl(2, 3));
+/* console.log(addExpr(2, 3));
+console.log(addArrow(2, 3)); */
 
 /* function addDecl(a, b) {
   return a + b;
@@ -51,56 +56,76 @@ var addExpr = function (a, b) {
   return a + b;
 };
 
-const addArrow = (a, b) => a + b;
+var addArrow = (a, b) => a + b; */
 
 //Example
-if (!numProducts) deleteShoppingCart();
+/* console.log(numProducts);
+if (!numProducts) deleteShoopingCart();
 
-var numProducts = 0;
+var numProducts = 10;
 
-function deleteShoppingCart() {
+function deleteShoopingCart() {
   console.log('All products deleted');
 }
 
 var x = 1;
 let y = 2;
-const z = 3;
- */
+const z = 3; */
 
-//This
 /* console.log(this);
+
 const calcAge = function (birthYear) {
   console.log(2037 - birthYear);
   console.log(this);
 };
-
 calcAge(1987);
- */
 
-/* var firstName = 'Matilda';
+const calcAgeArrow = birthYear => {
+  console.log(2037 - birthYear);
+  console.log(this);
+};
+calcAgeArrow(1991);
 
 const fernando = {
-  firstName: 'Fernando',
-  year: 1991,
+  year: 1987,
+  calcAge: function () {
+    console.log(this);
+    console.log(2037 - this.year);
+  },
+};
+fernando.calcAge();
+
+const matilda = {
+  year: 2017,
+};
+
+matilda.calcAge = fernando.calcAge;
+matilda.calcAge();
+
+const f = fernando.calcAge; */
+
+/* var firstName = 'Fernando'; */
+
+/* const fernando = {
+  firstName: 'Luis',
+  year: 1987,
   calcAge: function () {
     console.log(2037 - this.year);
 
     const isMillenial = () => {
-      console.log(self.year >= 1981 && self.year <= 1996);
+      console.log(this.year >= 1981 && this.year <= 1996);
     };
-
     isMillenial();
   },
+
   greet: () => {
     console.log(`Hey ${this.firstName}`);
   },
 };
- */
-//fernando.greet();
-//fernando.calcAge();
+fernando.greet();
+fernando.calcAge();
 
-//Arguments keyword
-/* const addExpr = function (a, b) {
+const addExpr = function (a, b) {
   console.log(arguments);
   return a + b;
 };
@@ -108,14 +133,19 @@ const fernando = {
 addExpr(2, 5);
 addExpr(2, 5, 8, 12);
 
-var addArrow = (a, b) => a + b; */
+var addArrow = (a, b) => {
+  return a + b;
+};
 
-/* let age = 30;
+addArrow(2, 5, 8);
+ */
+
+//Primitives and Objects
+let age = 30;
 let oldAge = age;
 age = 31;
-
-console.log('age: ' + age);
-console.log('old age: ' + oldAge);
+age;
+oldAge;
 
 const me = {
   name: 'Fernando',
@@ -123,45 +153,7 @@ const me = {
 };
 
 const friend = me;
-friend.age = 44;
-me;
- */
+friend.age = 27;
 
-/* let lastName = 'Salas';
-let oldLastName = lastName;
-
-lastName = 'Gave';
-
-console.log(lastName);
-console.log(oldLastName); */
-
-const luis = {
-  firstName: 'Luis',
-  lastName: 'Salas',
-  age: 34,
-};
-
-const marriedLuis = luis;
-marriedLuis.lastName = 'Gave';
-
-console.log('Before marriage', luis);
-console.log('After marriage', marriedLuis);
-
-//Copying objects
-const luis2 = {
-  firstName: 'Luis',
-  lastName: 'Salas',
-  age: 34,
-  family: ['maria', 'paola'],
-};
-
-const luisCoppy = Object.assign({}, luis2);
-luisCoppy.lastName = 'Sagan';
-console.log('Before marriage', luis2);
-console.log('After marriage', luisCoppy);
-
-luisCoppy.family.push('milagros');
-luisCoppy.family.push('claudia');
-
-console.log('Before marriage', luis2);
-console.log('After marriage', luisCoppy);
+console.log('Friend', friend);
+console.log('Me', me);
