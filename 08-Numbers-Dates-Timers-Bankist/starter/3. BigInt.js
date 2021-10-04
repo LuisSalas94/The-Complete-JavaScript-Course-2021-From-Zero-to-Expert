c6525d19500cc74f24ede97397d866ad8395c0ae
Fernando <luisfernandosalasg@gmail.com>
2021-10-04 11:12:42 -0500

    Add Dates & Timers section

---
+++ 08-Numbers-Dates-Timers-Bankist/starter/3. BigInt.js
@@ -0,0 +1,9 @@
+console.log(2 ** 53 - 1);
+console.log(Number.MAX_SAFE_INTEGER);
+console.log(2 ** 53 - 1);
+
+console.log(typeof 65465165115651515151651651651651651651651115516n);
+console.log(BigInt(65465165115651515151651651651651651651651115516n));
+
+//operations
+console.log(10000n + 10000n);
---
+++ 08-Numbers-Dates-Timers-Bankist/starter/4. creatingDates.js
@@ -0,0 +1,86 @@
+//Create Date
+
+const now = new Date();
+now;
+console.log(new Date('December 24, 2021'));
+
+const account1 = {
+  owner: 'Jonas Schmedtmann',
+  movements: [200, 455.23, -306.5, 25000, -642.21, -133.9, 79.97, 1300],
+  interestRate: 1.2, // %
+  pin: 1111,
+
+  movementsDates: [
+    '2019-11-18T21:31:17.178Z',
+    '2019-12-23T07:42:02.383Z',
+    '2020-01-28T09:15:04.904Z',
+    '2020-04-01T10:17:24.185Z',
+    '2020-05-08T14:11:59.604Z',
+    '2020-07-26T17:01:17.194Z',
+    '2020-07-28T23:36:17.929Z',
+    '2020-08-01T10:51:36.790Z',
+  ],
+  currency: 'EUR',
+  locale: 'pt-PT', // de-DE
+};
+
+const account2 = {
+  owner: 'Jessica Davis',
+  movements: [5000, 3400, -150, -790, -3210, -1000, 8500, -30],
+  interestRate: 1.5,
+  pin: 2222,
+
+  movementsDates: [
+    '2019-11-01T13:15:33.035Z',
+    '2019-11-30T09:48:16.867Z',
+    '2019-12-25T06:04:23.907Z',
+    '2020-01-25T14:18:46.235Z',
+    '2020-02-05T16:33:06.386Z',
+    '2020-04-10T14:43:26.374Z',
+    '2020-06-25T18:49:59.371Z',
+    '2020-07-26T12:01:20.894Z',
+  ],
+  currency: 'USD',
+  locale: 'en-US',
+};
+/* 
+const accounts = [account1, account2];
+console.log(new Date(account1.movementsDates[0]));
+
+console.log(new Date(2037, 10, 19, 15, 23, 5));
+console.log(new Date(2037, 10, 31, 15, 23, 5));
+console.log(new Date(3 * 24 * 60 * 60 * 1000)); */
+
+//Working wiht Dates
+/* const future = new Date(2037, 10, 19, 15, 23);
+future;
+console.log(future.getFullYear());
+console.log(future.getMonth());
+console.log(future.getDate());
+console.log(future.getDay());
+console.log(future.getHours());
+console.log(future.getMinutes());
+console.log(future.getSeconds());
+console.log(future.toISOString());
+console.log(future.getTime());
+console.log(Date.now());
+future.setFullYear(2040);
+console.log(future); */
+
+//Opetations With Date
+const future = new Date(2037, 10, 19, 15, 23);
+console.log(+future);
+
+const calcDaysPasssed = (date1, date2) =>
+  Math.round(Math.abs(date2 - date1) / (1000 * 60 * 60 * 24));
+
+const days1 = calcDaysPasssed(
+  new Date(2037, 3, 4),
+  new Date(2037, 3, 14, 10, 8)
+);
+days1;
+
+const num = 4343434.23;
+console.log('US: ', new Intl.NumberFormat('en-US').format(num));
+console.log('GERMANY: ', new Intl.NumberFormat('de-DE').format(num));
+console.log('Syria: ', new Intl.NumberFormat('ar-SY').format(num));
